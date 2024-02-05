ef0a200b55407c5843ce92cd8d7b07e1bf7c7c56
ShiniShiho <nakxkri@shinishiho.cyou>
2024-02-05 11:26:10 +0700

    add pkgs

--- system/pkgs.nix
+++ system/pkgs.nix
@@ -17,6 +17,41 @@
   };
 
   environment.systemPackages = with pkgs; [
+    # Prelogin
     greetd.tuigreet
+
+    # Utility
+    topgrade
+    ripgrep
+    skim
+    sd
+    fd
+    jq
+    mdcat
+    aria
+
+    # Tool
+    git
+    chezmoi
+    macchina
+    lazygit
+    ipfetch
+    cpufetch
+    starfetch
+    octofetch
+    bat
+    btop
+    brightnessctl
+    playerctl
+    cliphist
+    wl-clipboard
+    speedtest-rs
+
+    # Ricing
+    waybar
+    rofi-wayland
+    swww
+    wlogout
+    swayidle
   ];
 }
--- system/user.nix
+++ system/user.nix
@@ -9,12 +9,22 @@
     extraGroups = [ "networkmanager" "input" "wheel" "video" "audio" "tss" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
+      # Terminal
+      kitty
+      tmux
+      pokemon-colorscripts-mac
+      zsh-powerlevel10k
+
+      # Entertainment
+      qutebrowser
+      imv
+      mpv
+      ani-cli
       spotify
       discord
       tdesktop
       brave
-      zsh-powerlevel10k
-      pokemon-colorscripts-mac
+ 
     ];
   };
 
